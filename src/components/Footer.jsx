89901e6d3ee0a22504f9d0ceed0dde99f1fe667b
Huziafaf45 <huzaifaf45@gmail.com>
2024-08-08 22:48:49 +0500

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>© 2023 by Huzaifa Farooq. All rights reserved.</p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
+
+
